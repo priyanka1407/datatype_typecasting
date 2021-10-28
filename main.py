a71985aa6c3e3f3606e635b861fa12cb443b8643
Priyanka0706 <priyankamurugavel@gmail.com>
2021-10-28 05:06:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+two_digit_number1=int(two_digit_number[0])+int(two_digit_number[1])
+
+print(type(two_digit_number))
+print(str(two_digit_number[0])+"+"+str(two_digit_number[1])+"=")
+print(two_digit_number1)
+
+
+
+
+
+
+
+
